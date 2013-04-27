9d96547dbc1276f8f4eca6fc14eabf460ccbb69b
Chris Barrett <chris.d.barrett@me.com>
2013-04-27 15:28:01 +1200

    Rename prefix slc -> scl

--- sclang-ac-mode.el
+++ sclang-ac-mode.el
@@ -70,7 +70,7 @@
 
 ;;; ----------------------------------------------------------------------------
 
-(defmacro slc:logged (&rest body)
+(defmacro scl:logged (&rest body)
   "Like `progn', but logs the result to messages if `sclang-ac-verbose' is non-nil."
   (declare (indent 0))
   (let ((result (cl-gensym)))
@@ -79,7 +79,7 @@
          (message "[sclang-ac]: %s" ,result))
        ,result)))
 
-(defun* slc:blocking-eval-string (expr &optional (timeout-ms 50))
+(defun* scl:blocking-eval-string (expr &optional (timeout-ms 50))
   "Ask SuperCollider to evaluate the given string EXPR. Wait a maximum TIMEOUT-MS."
   (unless (s-blank? expr)
     (let ((result nil)
@@ -100,7 +100,7 @@
           (setq elapsed (+ 10 elapsed)))
         result))))
 
-(defun slc:deserialize (str)
+(defun scl:deserialize (str)
   "Parse the SuperCollider response STR."
   (when str
     (->> str
@@ -110,95 +110,95 @@
       (s-replace "]" ")")
       (read))))
 
-(defun slc:request (format-string &rest args)
+(defun scl:request (format-string &rest args)
   "Define a blocking request to SuperCollider.
 Empty responses are returned as nil."
   (let ((response
-         (slc:deserialize
-          (slc:blocking-eval-string
+         (scl:deserialize
+          (scl:blocking-eval-string
            (apply 'format format-string args)))))
     (if (and (stringp response)
              (s-blank? response))
         nil
       response)))
 
-(defun slc:methods (class)
+(defun scl:methods (class)
   "Return a list of methods implemented by CLASS."
   (unless (s-blank? class)
-    (slc:request "%s.methods.collect {|m| [m.name, m.argList, m.ownerClass] }"
+    (scl:request "%s.methods.collect {|m| [m.name, m.argList, m.ownerClass] }"
                  class)))
 
-(defun slc:all-methods (class)
+(defun scl:all-methods (class)
   "Return a list of methods implemented by CLASS and its superclasses."
   (unless (s-blank? class)
-    (->> (cons class (slc:superclasses class))
-      (-mapcat 'slc:methods)
+    (->> (cons class (scl:superclasses class))
+      (-mapcat 'scl:methods)
       (-uniq))))
 
-(defun slc:instance-vars (class)
+(defun scl:instance-vars (class)
   "Return a list of the instance variables of CLASS."
   (unless (s-blank? class)
-    (slc:request "%s.instVarNames.collect(_.asString)" class)))
+    (scl:request "%s.instVarNames.collect(_.asString)" class)))
 
-(defun slc:class-vars (class)
+(defun scl:class-vars (class)
   "Return a list of the class variables of CLASS."
   (unless (s-blank? class)
-    (slc:request "%s.classVarNames.collect(_.asString)" class)))
+    (scl:request "%s.classVarNames.collect(_.asString)" class)))
 
-(defun slc:superclasses (class)
+(defun scl:superclasses (class)
   "Return a list of superclasses for CLASS."
   (unless (s-blank? class)
-    (-map 'symbol-name (slc:request "%s.superclasses" class))))
+    (-map 'symbol-name (scl:request "%s.superclasses" class))))
 
-(defun slc:subclasses (class)
+(defun scl:subclasses (class)
   "Return the direct subclasses of CLASS."
   (unless (s-blank? class)
-    (-map 'symbol-name (slc:request "%s.subclasses" class))))
+    (-map 'symbol-name (scl:request "%s.subclasses" class))))
 
-(defun slc:class-summary (class)
+(defun scl:class-summary (class)
   "Return the summary for the given class."
   (unless (s-blank? class)
-    (slc:request "SCDoc.documents[\"Classes/%s\"].summary" class)))
+    (scl:request "SCDoc.documents[\"Classes/%s\"].summary" class)))
 
-(defun slc:class-of (expr)
+(defun scl:class-of (expr)
   "Evaluate EXPR and return the class of the result."
   (unless (s-blank? expr)
-    (slc:blocking-eval-string (format "(%s).class" expr))))
+    (scl:blocking-eval-string (format "(%s).class" expr))))
 
-(defun slc:ensure-non-meta-class (class)
+(defun scl:ensure-non-meta-class (class)
   "Make sure that the given CLASS name is not prefixed by Meta_.
 This is necessary when looking up documentation, because class
 methods are actually instance methods of the meta-class."
   (s-chop-prefix "Meta_" class))
 
-(defun slc:method-arg-info (class method-name)
+(defun scl:method-arg-info (class method-name)
   "Get the name and description of each argument for a method. "
-  (let ((k (slc:ensure-non-meta-class class)))
+  (let ((k (scl:ensure-non-meta-class class)))
     (or
      ;; Try class method.
-     (slc:request
+     (scl:request
       (concat "SCDoc.getMethodDoc(\"%s\", \"*%s\")"
               ".findChild(\\METHODBODY)"
               ".findChild(\\ARGUMENTS).children.collect{|x| "
               "[x.text, x.findChild(\\PROSE).findChild(\\TEXT).text] "
               "} ") k method-name)
      ;; Try instance method.
-     (slc:request
+     (scl:request
       (concat "SCDoc.getMethodDoc(\"%s\", \"-%s\")"
               ".findChild(\\METHODBODY)"
               ".findChild(\\ARGUMENTS).children.collect{|x| "
               "[x.text, x.findChild(\\PROSE).findChild(\\TEXT).text] " "} ")
       k method-name))))
 
-(defun slc:all-classes ()
+(defun scl:all-classes ()
   "Return the list of classes known to SuperCollider."
   (->> "Class.allClasses.asArray"
-      (slc:blocking-eval-string)
+      (scl:blocking-eval-string)
       (s-replace "class" "")
-      (slc:deserialize)
+      (scl:deserialize)
       (-map 'symbol-name)))
 
-(defun slc:looking-at-member-access? ()
+(defun scl:looking-at-member-access? ()
   "Return point if not looking at a member access."
   (when (s-contains? "." (thing-at-point 'line ))
     (point)))
@@ -207,17 +207,17 @@ methods are actually instance methods of the meta-class."
 ;;; Completion sources.
 ;;
 ;; Completion sources that require a reference to a class (i.e. methods,
-;; instance vars) use the `slc:last-class' variable. This ensures that we know
+;; instance vars) use the `scl:last-class' variable. This ensures that we know
 ;; which class to operate on regardless of buffer insertions caused by the
 ;; completion.
 
-(defvar slc:last-class nil
+(defvar scl:last-class nil
   "The class to use for completion candidates.")
 
-(defconst slc:bullet "•")
-(defconst slc:ellipsis "…")
+(defconst scl:bullet "•")
+(defconst scl:ellipsis "…")
 
-(defun* slc:ellipsize (str &optional (maxlen 30))
+(defun* scl:ellipsize (str &optional (maxlen 30))
   "Ellipsize string STR if it is longer than MAXLEN."
   (cond
 
@@ -231,24 +231,24 @@ methods are actually instance methods of the meta-class."
       (butlast)
       (s-join " ")
       (s-prepend "[")
-      (s-append (format " %s]" slc:ellipsis))))
+      (s-append (format " %s]" scl:ellipsis))))
 
    ;; Trim and ellipsize.
    (t
-    (s-append slc:ellipsis (substring str 0 (1- maxlen))))))
+    (s-append scl:ellipsis (substring str 0 (1- maxlen))))))
 
-(slc:ellipsize "(the quick brown fox jumps over the lazy dog ho ho ho ho ho ho)")
+(scl:ellipsize "(the quick brown fox jumps over the lazy dog ho ho ho ho ho ho)")
 
-(defun slc:method-bullets (method-arg-info)
+(defun scl:method-bullets (method-arg-info)
   "Build a bulleted list describing a method's arguments."
   (when method-arg-info
     (format "\n\narguments:%s"
             (->> method-arg-info
               (--map (format "%s: \t%s" (car it) (cadr it)))
-              (s-join (format "\n\n%s " slc:bullet))
-              (s-prepend (format "\n%s " slc:bullet))))))
+              (s-join (format "\n\n%s " scl:bullet))
+              (s-prepend (format "\n%s " scl:bullet))))))
 
-(defun* slc:selected-method-doc ((arglist owner)
+(defun* scl:selected-method-doc ((arglist owner)
                                  &optional (name (ac-selected-candidate)))
   "Show documentation for the currently selected method in the `ac-menu'."
   (s-concat
@@ -257,125 +257,125 @@ methods are actually instance methods of the meta-class."
    ;; Display arglist.
    (unless (s-blank? arglist) arglist)
    ;; Display arglist details.
-   (slc:method-bullets (slc:method-arg-info owner name))))
+   (scl:method-bullets (scl:method-arg-info owner name))))
 
-(defun* slc:method-item ((name arglist owner))
+(defun* scl:method-item ((name arglist owner))
   "Stringify and process the elements of an sclang method item."
   (let ((sym (eval name)))
     (when (symbolp sym)
       (list (symbol-name sym)
             (eval arglist)
-            (slc:ensure-non-meta-class (symbol-name owner))))))
+            (scl:ensure-non-meta-class (symbol-name owner))))))
 
-(defun* slc:class-doc-subclasses (class &optional (maxlen 5))
+(defun* scl:class-doc-subclasses (class &optional (maxlen 5))
   "Return a list of subclasses. It will be ellipsized if longer than MAXLEN"
-  (let* ((subclasses (slc:subclasses class))
+  (let* ((subclasses (scl:subclasses class))
          ;; Show MAXLEN subclasses before ellipsizing.
          (sub-str (->> subclasses
                     (-take maxlen)
-                    (s-join slc:bullet)
-                    (s-prepend slc:bullet)))
+                    (s-join scl:bullet)
+                    (s-prepend scl:bullet)))
          (sub-str (if (< maxlen (length subclasses))
                       (s-append "\n  …" sub-str)
                     sub-str)))
     (when subclasses
       (concat "\n\nsubclasses:" sub-str))))
 
-(defun slc:class-documentation (class)
+(defun scl:class-documentation (class)
   "Create an auto-complete documentation for CLASS."
-  (let ((super (s-join " < " (slc:superclasses class))))
+  (let ((super (s-join " < " (scl:superclasses class))))
     (s-concat
      class
      ;; Summarize class.
-     (-when-let (summary (slc:class-summary class))
+     (-when-let (summary (scl:class-summary class))
        (concat ":\n" summary))
      ;; Display inheritance chain.
      (unless (s-blank? super)
        (format "\n\ninheritance chain:\n%s < %s" class super))
      ;; List subclasses.
-     (slc:class-doc-subclasses class))))
+     (scl:class-doc-subclasses class))))
 
-(defun slc:class-defines? (class name)
+(defun scl:class-defines? (class name)
   "Return a cons of (CLASS . NAME) if CLASS defines a method or var NAME."
-  (when (-> (-concat (slc:instance-vars class) (slc:methods class))
+  (when (-> (-concat (scl:instance-vars class) (scl:methods class))
           (-contains? name))
     (cons class name)))
 
-(defun slc:find-declaring-class (class name)
+(defun scl:find-declaring-class (class name)
   "Walk the class hierarchy from CLASS, searching for which class defines NAME."
-  (->> (cons class (slc:superclasses class))
+  (->> (cons class (scl:superclasses class))
     (reverse)
-    (--first (slc:class-defines? it name))))
+    (--first (scl:class-defines? it name))))
 
-(defun* slc:selected-var-doc
-    (var-name &optional (class slc:last-class))
+(defun* scl:selected-var-doc
+    (var-name &optional (class scl:last-class))
   "Get the documentation for VAR-NAME."
-  (let ((qual-sym (format "%s.%s" (slc:find-declaring-class class var-name) var-name)))
+  (let ((qual-sym (format "%s.%s" (scl:find-declaring-class class var-name) var-name)))
     (s-concat
      ;; CLASS.NAME
      qual-sym
      ;; Show value if possible.
-     (->> (slc:request "%s.%s" (slc:ensure-non-meta-class class) var-name)
+     (->> (scl:request "%s.%s" (scl:ensure-non-meta-class class) var-name)
        (prin1-to-string)
-       (slc:ellipsize)
+       (scl:ellipsize)
        (concat "\n\nvalue: ")))))
 
 (ac-define-source sclang-classes
-  '((candidates . (slc:logged
-                    (unless (slc:looking-at-member-access?)
-                      (slc:all-classes))))
-    (document   . slc:class-documentation)
+  '((candidates . (scl:logged
+                    (unless (scl:looking-at-member-access?)
+                      (scl:all-classes))))
+    (document   . scl:class-documentation)
     (symbol     . "s")
     (limit      . nil)))
 
 (ac-define-source sclang-toplevel-functions
-  '((candidates . (slc:logged
-                    (unless (slc:looking-at-member-access?)
-                      (-map 'slc:method-item (slc:methods "AbstractFunction")))))
-    (document   . slc:selected-method-doc)
+  '((candidates . (scl:logged
+                    (unless (scl:looking-at-member-access?)
+                      (-map 'scl:method-item (scl:methods "AbstractFunction")))))
+    (document   . scl:selected-method-doc)
     (symbol     . "f")
     (limit      . nil)))
 
 (ac-define-source sclang-methods
-  '((candidates . (slc:logged
-                    (->> (slc:all-methods slc:last-class)
-                      (-map 'slc:method-item)
+  '((candidates . (scl:logged
+                    (->> (scl:all-methods scl:last-class)
+                      (-map 'scl:method-item)
                       (-remove 'null))))
-    (document   . slc:selected-method-doc)
+    (document   . scl:selected-method-doc)
     (prefix     . ac-prefix-default)
     (symbol     . "f")
     (limit      . nil)
     (requires   . -1)))
 
 (ac-define-source sclang-ivars
-  '((candidates . (slc:logged
-                    (slc:instance-vars slc:last-class)))
+  '((candidates . (scl:logged
+                    (scl:instance-vars scl:last-class)))
     (prefix     . ac-prefix-default)
-    (document   . slc:selected-var-doc)
+    (document   . scl:selected-var-doc)
     (symbol     . "v")
     (limit      . nil)
     (requires   . -1)))
 
 ;;; ----------------------------------------------------------------------------
 
-(defun slc:open-brace-pos ()
+(defun scl:open-brace-pos ()
   "Find the position of the first preceding opening brace."
   (save-excursion
     (search-backward-regexp (rx (any "(" "[" "{"))
                             (line-beginning-position)
                             t)))
 
-(defun slc:between? (n start end)
+(defun scl:between? (n start end)
   "Non-nil if N is between START and END, inclusively."
   (and (>= n start) (<= n end)))
 
-(defun* slc:expression-start-pos (&optional (pt (point)))
+(defun* scl:expression-start-pos (&optional (pt (point)))
   "Return the start of the current sclang expression."
   (save-excursion
     (goto-char pt)
     (let* ((bol (line-beginning-position))
            (semicolon (save-excursion (search-backward ";" bol t)))
-           (open-brace (slc:open-brace-pos))
+           (open-brace (scl:open-brace-pos))
 
            ;; Find the extents of the nearest preceding braced expression.
            (brace-end (save-excursion
@@ -391,13 +391,13 @@ methods are actually instance methods of the meta-class."
            (semicolon-at-this-nesting?
             (ignore-errors
               (and (> semicolon open-brace)
-                   (not (slc:between? semicolon brace-start brace-end))))))
+                   (not (scl:between? semicolon brace-start brace-end))))))
       (or
        ;; Skip backwards over braced expressions and continue.
        (when (thing-at-point-looking-at (rx (any "}" "]" ")" "\"") (* space)))
          (save-excursion
            (backward-sexp)
-           (slc:expression-start-pos (point))))
+           (scl:expression-start-pos (point))))
 
        ;; Check for semicolon at this level of nesting.
        (when (and semicolon semicolon-at-this-nesting?)
@@ -410,14 +410,14 @@ methods are actually instance methods of the meta-class."
        ;; Otherwise, fall back to using the whole line.
        (line-beginning-position)))))
 
-(defun slc:class-of-thing-at-point ()
+(defun scl:class-of-thing-at-point ()
   "Return the class of the sclang expression at point."
-  (slc:logged
-    (->> (buffer-substring-no-properties (slc:expression-start-pos) (point))
+  (scl:logged
+    (->> (buffer-substring-no-properties (scl:expression-start-pos) (point))
       (s-trim)
       ;; Remove trailing dot-accessor.
       (s-chop-suffix ".")
-      (slc:class-of))))
+      (scl:class-of))))
 
 ;;;###autoload
 (defun sclang-electric-dot ()
@@ -425,8 +425,8 @@ methods are actually instance methods of the meta-class."
   (interactive)
 
   ;; Update the reference to the last class before starting completion.
-  (-when-let (k (slc:class-of-thing-at-point))
-    (setq slc:last-class k))
+  (-when-let (k (scl:class-of-thing-at-point))
+    (setq scl:last-class k))
 
   (insert ".")
 
@@ -437,7 +437,7 @@ methods are actually instance methods of the meta-class."
 (defun sclang-expression-start ()
   "Move to the start of the sclang expression before point."
   (interactive)
-  (goto-char (slc:expression-start-pos)))
+  (goto-char (scl:expression-start-pos)))
 
 (defvar sclang-ac-mode-map
   (let ((map (make-keymap)))
